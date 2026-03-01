5e642a2382eb2406a650c4c8c93d6cdbb480cc4e
Severi Reivinen <severi.reivinen@gmail..com>
2026-03-01 12:54:19 +0200

    fix: build

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# Use sed to replace variables. 
-# Note: This is a simple replacement. If your passwords have special characters like '/', 
-# you might need to use a different delimiter or stick to Option 2.
-sed "s|\${VOIMA_METRICS_USER}|$VOIMA_METRICS_USER|g" /etc/prometheus/prom.yml.template > /etc/prometheus/prom.yml
-sed -i "s|\${VOIMA_METRICS_PASSWORD}|$VOIMA_METRICS_PASSWORD|g" /etc/prometheus/prom.yml
 
-cp /etc/prometheus/prom.yml.template /etc/prometheus/prom.yml
+# Use 'envsubst' if you can get it to work, BUT 
+# here is the 100% shell-safe way using 'sed' with a unique delimiter:
+# We use ASCII character \001 as a delimiter because it's never in a password.
+
+DELIM=$(printf '\001')
+
+sed -i "s${DELIM}\${VOIMA_METRICS_USER}${DELIM}${VOIMA_METRICS_USER}${DELIM}g" /etc/prometheus/prom.yml
+sed -i "s${DELIM}\${VOIMA_METRICS_PASSWORD}${DELIM}${VOIMA_METRICS_PASSWORD}${DELIM}g" /etc/prometheus/prom.yml
 
 exec /bin/prometheus "$@"
