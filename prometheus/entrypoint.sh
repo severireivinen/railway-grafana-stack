adc0cbb269b242c36605de22227b56c8527c7783
Severi Reivinen <severi.reivinen@gmail..com>
2026-03-01 12:36:35 +0200

    fix: build

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# Substitute environment variables into the Prometheus config template
-envsubst < /etc/prometheus/prom.yml.template > /etc/prometheus/prom.yml
+# Use sed to replace variables. 
+# Note: This is a simple replacement. If your passwords have special characters like '/', 
+# you might need to use a different delimiter or stick to Option 2.
+sed -i "s/\${VOIMA_METRICS_USER}/$VOIMA_METRICS_USER/g" /etc/prometheus/prom.yml.template
+sed -i "s/\${VOIMA_METRICS_PASSWORD}/$VOIMA_METRICS_PASSWORD/g" /etc/prometheus/prom.yml.template
 
-# Start Prometheus with the processed config
+cp /etc/prometheus/prom.yml.template /etc/prometheus/prom.yml
+
 exec /bin/prometheus "$@"
